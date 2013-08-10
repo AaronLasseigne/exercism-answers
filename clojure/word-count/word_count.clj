052bdce7a0705f7695b308461b40811a5d7cce47
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-08-09 20:10:00 -0500

    normalize the text rather than the individual words for word-count in clojure

--- clojure/word-count/word_count.clj
+++ clojure/word-count/word_count.clj
@@ -1,14 +1,11 @@
 (ns phrase
   (:require [clojure.string :as str :only [split lower-case]]))
 
-(defn- words [text]
-  (str/split text #"[^\p{Alnum}]+"))
-
 (defn- normalize [word]
   (str/lower-case word))
 
+(defn- words [text]
+  (str/split (normalize text) #"[^\p{Alnum}]+"))
+
 (defn word-count [text]
-  (->>
-    (words text)
-    (map normalize)
-    (frequencies)))
+  (frequencies (words text)))
