2668344955e9dda21ee83e493fba05dd8fc92f36
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-08-18 18:54:15 -0500

    let usually starts on the line with the let

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -2,6 +2,5 @@
 
 (defn anagrams-for
   [subject words]
-  (let
-    [subject-char-freq (frequencies subject)]
+  (let [subject-char-freq (frequencies subject)]
     (filter #(= subject-char-freq (frequencies %)) words)))
