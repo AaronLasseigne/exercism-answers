bdb076d0163f324c17caa0ddfdf5dc4ff12e5543
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-08-18 17:08:35 -0500

    use more common hyphen in var name

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -3,5 +3,5 @@
 (defn anagrams-for
   [subject words]
   (let
-    [subject_char_freq (frequencies subject)]
-    (filter #(= subject_char_freq (frequencies %)) words)))
+    [subject-char-freq (frequencies subject)]
+    (filter #(= subject-char-freq (frequencies %)) words)))
