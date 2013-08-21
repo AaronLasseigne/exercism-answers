e18faf0d9f4ee8c21adf3fc6e2cffdd475a13e0b
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-08-20 21:31:42 -0500

    a more OO approach to dna in ruby

--- ruby/nucleotide-count/dna.rb
+++ ruby/nucleotide-count/dna.rb
@@ -1,30 +1,81 @@
-class DNA
-  NUCLEOTIDES = %w[A T G C].freeze
+module Nucleotide
+  class Base
+    def self.frequency(chain)
+      chain.count(letter)
+    end
 
-  def initialize(dna)
-    @dna    = dna
-    @counts = {}
+    def self.letter
+      class_name[0]
+    end
+
+    private
+
+    def self.class_name
+      name[/::([a-z]+)\z/i, 1]
+    end
   end
 
-  def count(nucleotide)
-    if NUCLEOTIDES.include?(nucleotide)
-      counts[nucleotide] ||= dna.count(nucleotide)
-    elsif nucleotide == 'U'
-      0
-    else
-      raise ArgumentError
+  class Adenine  < Base; end
+  class Cytosine < Base; end
+  class Guanine  < Base; end
+  class Thymine  < Base; end
+  class Uracil   < Base; end
+
+  ALL = [Adenine, Cytosine, Guanine, Thymine, Uracil].freeze
+
+  def self.find_by_letter(letter)
+    nucleotide = ALL.detect do |nucleotide|
+      nucleotide.letter == letter
     end
+
+    if not nucleotide
+      raise ArgumentError, %Q("#{letter}" is not a valid representation of a nucldeotide.)
+    end
+
+    nucleotide
+  end
+end
+
+class DNA
+  NUCLEOTIDES = [
+    Nucleotide::Adenine,
+    Nucleotide::Cytosine,
+    Nucleotide::Guanine,
+    Nucleotide::Thymine
+  ].freeze
+  NUCLEOTIDE_LETTERS = NUCLEOTIDES.map(&:letter).freeze
+
+  def initialize(letters)
+    @letters = letters
+    @letter_counts = {}
+  end
+
+  def count(letter)
+    return letter_counts[letter] if letter_counts[letter]
+
+    nucleotide = Nucleotide.find_by_letter(letter)
+    frequency = nucleotide.frequency(letters)
+
+    if NUCLEOTIDES.include?(nucleotide)
+      letter_counts[letter] = frequency
+    end
+
+    frequency
   end
 
   def nucleotide_counts
-    (NUCLEOTIDES - counts.keys).each do |nucleotide|
-      count(nucleotide)
+    uncounted_letters.each do |letter|
+      count(letter)
     end
 
-    counts
+    letter_counts
   end
 
   private
 
-  attr_reader :dna, :counts
+  attr_reader :letters, :letter_counts
+
+  def uncounted_letters
+    (NUCLEOTIDE_LETTERS - letter_counts.keys)
+  end
 end
