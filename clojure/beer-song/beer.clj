51c9243b9e8c297a4d7e1746f598bc168eb5834f
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-08-22 14:31:30 -0500

    format! a function to save the day

--- clojure/beer-song/beer.clj
+++ clojure/beer-song/beer.clj
@@ -4,44 +4,42 @@
 (defn- bottles
   [remaining]
   (case remaining
-    (-1) "99 bottles"
-    (0)  "no more bottles"
-    (1)  "1 bottle"
-    (join " " [remaining "bottles"])))
+    (0) "no more bottles"
+    (1) "1 bottle"
+    (format "%d bottles" remaining)))
 
 (defn- first-line
   [remaining]
   (let [bottles_remaining (bottles remaining)]
-    (join " " [(capitalize bottles_remaining)
-               "of beer on the wall,"
-               bottles_remaining
-               "of beer."])))
+    (format "%s of beer on the wall, %s of beer.\n"
+            (capitalize bottles_remaining)
+            bottles_remaining)))
 
 (defn- second-line-first-half
   [remaining]
   (if (= 0 remaining)
     "Go to the store and buy some more"
-    (join " " ["Take"
-               (if (= 1 remaining) "it" "one")
-               "down and pass it around"])))
+    (format "Take %s down and pass it around"
+            (if (= 1 remaining) "it" "one"))))
 
 (defn- second-line-second-half
   [remaining]
-  (join " " ["," (bottles (dec remaining)) "of beer on the wall."]))
+  (format ", %s of beer on the wall.\n"
+          (bottles (if (= -1 remaining) 99 remaining))))
 
 (defn- second-line
   [remaining]
   (apply str
-    (second-line-first-half remaining)
-    (second-line-second-half remaining)))
+         (second-line-first-half remaining)
+         (second-line-second-half (dec remaining))))
 
 (defn verse
   [bottles]
-  (join "\n" [(first-line bottles) (second-line bottles) ""]))
+  (apply str (first-line bottles) (second-line bottles)))
 
 (defn sing
   ([beginning] (sing beginning 0))
   ([beginning ending]
-  (if (= beginning ending)
-    (apply str (verse beginning) "\n")
-    (join "\n" [(verse beginning) (sing (dec beginning) ending)]))))
+   (if (= beginning ending)
+     (apply str (verse beginning) "\n")
+     (join "\n" [(verse beginning) (sing (dec beginning) ending)]))))
