d38044295a1eb9b8865560c1a5cfa5e994f94db9
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-10-02 13:26:56 -0500

    use count instead of foldl

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -2,12 +2,10 @@ defmodule DNA do
   def hamming_distance(strand, other) do
     [strand, other]
     |> List.zip
-    |> List.foldl 0, fn (nucleotides, difference) ->
-      difference + if matching_pair?(nucleotides), do: 0, else: 1
-    end
+    |> Enum.count &non_matching_pair?/1
   end
 
-  defp matching_pair?(nucleotides) do
-    elem(nucleotides, 0) == elem(nucleotides, 1)
+  defp non_matching_pair?(nucleotides) do
+    elem(nucleotides, 0) != elem(nucleotides, 1)
   end
 end
