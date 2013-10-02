b67d07c968a1acd90a284d1ad746cbd303ecacaa
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-10-02 11:48:21 -0500

    second pass, this time without recursion

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -1,15 +1,13 @@
 defmodule DNA do
   def hamming_distance(strand, other) do
-    hamming_distance(strand, other, 0)
+    [strand, other]
+    |> List.zip
+    |> List.foldl 0, fn (nucleotides, difference) ->
+      difference + if matching_pair?(nucleotides), do: 0, else: 1
+    end
   end
-  defp hamming_distance(_ , '', difference), do: difference
-  defp hamming_distance('', _ , difference), do: difference
-  defp hamming_distance(strand, other, difference) do
-    [nucleotide1 | rest_of_strand] = strand
-    [nucleotide2 | rest_of_other]  = other
 
-    count = difference + if nucleotide1 == nucleotide2, do: 0, else: 1
-
-    hamming_distance(rest_of_strand, rest_of_other, count)
+  defp matching_pair?(nucleotides) do
+    elem(nucleotides, 0) == elem(nucleotides, 1)
   end
 end
