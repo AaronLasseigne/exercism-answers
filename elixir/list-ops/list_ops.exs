98063edef07eb3654ac2bb7f059ec034c3ca2570
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-03-15 15:01:11 -0500

    initial answer for list opts in elixir

---
+++ elixir/list-ops/list_ops.exs
@@ -0,0 +1,55 @@
+defmodule ListOps do
+  # Please don't use any external modules (especially List) in your
+  # implementation. The point of this exercise is to create these basic functions
+  # yourself.
+  #
+  # Note that `++` is a function from an external module (Kernel, which is
+  # automatically important`) and so shouldn't be used either.
+
+  @spec count(list) :: non_neg_integer
+  def count(l) do
+    _count(l, 0)
+  end
+  defp _count([],    acc), do: acc
+  defp _count([_|t], acc), do: _count(t, acc + 1)
+
+  @spec reverse(list) :: list
+  def reverse(l) do
+    _reverse(l, [])
+  end
+  defp _reverse([],    acc), do: acc
+  defp _reverse([h|t], acc), do: _reverse(t, [h | acc])
+
+  @spec map(list, (any -> any)) :: list
+  def map(l, f) do
+    reverse(_map(l, f, []))
+  end
+  def _map([],    _, acc), do: acc
+  def _map([h|t], f, acc), do: _map(t, f, [f.(h) | acc])
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f) do
+    reverse(_filter(l, f, []))
+  end
+  defp _filter([],    _, acc), do: acc
+  defp _filter([h|t], f, acc) do
+    if f.(h) do
+      acc = [h | acc]
+    end
+
+    _filter(t, f, acc)
+  end
+
+  @type acc :: any
+  @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
+  def reduce([],    acc, _), do: acc
+  def reduce([h|t], acc, f), do: reduce(t, f.(h, acc), f)
+
+  @spec append(list, list) :: list
+  def append([],    b), do: b
+  def append([h|t], b), do: [h | append(t, b)]
+
+  @spec concat([[any]]) :: [any]
+  def concat([]),    do: []
+  def concat([h|t]), do: append(h, concat(t))
+end
