3e364e020ca747d08ca48fe1143e4b57d1a9d304
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-03-19 22:40:13 -0500

    initial solution for grade school in clojure

---
+++ clojure/grade-school/school.clj
@@ -0,0 +1,17 @@
+(ns school)
+
+(defn add
+  [db student grade_num]
+  (update-in db [grade_num] (fnil conj []) student))
+
+(defn grade
+  [db grade_num]
+  (get-in db [grade_num] []))
+
+(defn- sort-students
+  [students _]
+  (into [] (sort students)))
+
+(defn sorted
+  [db]
+  (merge-with sort-students db db))
