cf063028aaa53826559ce2d82e7863c2d1010790
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-04-24 20:00:31 -0500

    initial answer for sublist in elixir

---
+++ elixir/sublist/sublist.exs
@@ -0,0 +1,32 @@
+defmodule Sublist do
+  @doc """
+  Returns whether the first list is a sublist or a superlist of the second list
+  and if not whether it is equal or unequal to the second list.
+  """
+  def compare(a, b) do
+    cond do
+      a === b ->
+        :equal
+      contains(a, b) ->
+        :superlist
+      contains(b, a) ->
+        :sublist
+      true ->
+        :unequal
+    end
+  end
+
+  defp contains(full, sub) do
+    sub_count = Enum.count(sub)
+
+    cond do
+      sub_count > Enum.count(full) ->
+        false
+      Enum.take(full, sub_count) === sub ->
+        true
+      true ->
+        [_ | t] = full
+        contains(t, sub)
+    end
+  end
+end
