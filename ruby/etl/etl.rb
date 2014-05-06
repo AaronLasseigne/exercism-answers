55fc236ce2cf69157042e330fd7f7b84eae269ed
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-05-05 22:46:07 -0500

    the default is unnecessary

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,6 +1,6 @@
 class ETL
   def self.transform(data)
-    data.each_with_object(Hash.new(0)) do |(score, letters), h|
+    data.each_with_object({}) do |(score, letters), h|
       letters.each do |letter|
         h[letter.downcase] = score
       end
