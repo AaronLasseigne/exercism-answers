f14da45140bfbb3e72c76bbd462d4cfb586bbbe6
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-05-09 21:38:50 -0500

    initial answer to grade school in ruby

---
+++ ruby/grade-school/school.rb
@@ -0,0 +1,19 @@
+class School
+  def initialize
+    @roster = Hash.new { |h, k| h[k] = [] }
+  end
+
+  def add(name, level)
+    @roster[level].push(name)
+  end
+
+  def grade(level)
+    @roster[level].sort
+  end
+
+  def to_hash
+    @roster.sort.each_with_object({}) do |(level, names), h|
+      h[level] = names.sort
+    end
+  end
+end
