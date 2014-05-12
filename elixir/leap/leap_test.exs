90a5ab82a666b550739a99847a139cb44e2fa7c5
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-05-11 23:51:32 -0500

    working around exercism funkiness

--- elixir/leap/leap_test.exs
+++ elixir/leap/leap_test.exs
@@ -1,7 +1,7 @@
 if System.get_env("EXERCISM_TEST_EXAMPLES") do
   Code.load_file("example.exs")
 else
-  Code.load_file("year.exs")
+  Code.load_file("leap.exs")
 end
 
 ExUnit.start
@@ -15,14 +15,14 @@ defmodule LeapTest do
   end
 
   test "any old year" do
-    # assert ! Year.leap_year?(1997)
+    assert ! Year.leap_year?(1997)
   end
 
   test "century" do
-    # assert ! Year.leap_year?(1900)
+    assert ! Year.leap_year?(1900)
   end
 
   test "exceptional century" do
-    # assert Year.leap_year?(2400)
+    assert Year.leap_year?(2400)
   end
 end
