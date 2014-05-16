36768893cb2335867b9a294de97b14aea09f209d
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-05-16 17:04:23 -0500

    avoid side effects in validate

--- scala/nucleotide-count/src/main/scala/dna.scala
+++ scala/nucleotide-count/src/main/scala/dna.scala
@@ -1,15 +1,16 @@
 class DNA(strand: String) {
   private val nucleotides = Set('A', 'T', 'G', 'C')
 
-  private def validate(nucleotide: Char) =
-    nucleotides.contains(nucleotide) || (throw new IllegalArgumentException)
+  private def validate(nucleotide: Char) = nucleotides.contains(nucleotide)
 
-  strand.foreach(validate(_))
+  require(strand.forall(validate(_)))
 
   def count(nucleotide: Char) = nucleotide match {
     case 'U' => 0
     case nucleotide if validate(nucleotide) =>
       strand.count { _ == nucleotide }
+    case _ =>
+      throw new IllegalArgumentException
   }
 
   def nucleotideCounts =
