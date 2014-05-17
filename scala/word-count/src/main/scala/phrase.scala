969b107e599f708850995136d03dcf595fdc70ca
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-05-16 22:10:17 -0500

    use lazy val and make slightly nicer

--- scala/word-count/src/main/scala/phrase.scala
+++ scala/word-count/src/main/scala/phrase.scala
@@ -1,9 +1,11 @@
 class Phrase(phrase: String) {
-  private val words = phrase
-    .split("(?i)[^a-z0-9']+")
-    .map(_.toLowerCase)
+  private val words =
+    phrase
+      .split("(?i)[^a-z0-9']+")
+      .map(_.toLowerCase)
 
-  def wordCount = words
-    .groupBy(identity)
-    .mapValues(_.size)
+  lazy val wordCount =
+    words
+      .groupBy(identity)
+      .mapValues(_.size)
 }
