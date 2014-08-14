348925e4231e1005ddfd040a53eccfdc15c0cc3a
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-08-14 13:57:57 -0500

    use a sorted map

--- clojure/roman-numerals/roman_numerals.clj
+++ clojure/roman-numerals/roman_numerals.clj
@@ -1,19 +1,20 @@
 (ns roman_numerals)
 
 (def ^:private conversions
-  [[1000 "M"]
-   [900 "CM"]
-   [500 "D"]
-   [400 "CD"]
-   [100 "C"]
-   [90 "XC"]
-   [50 "L"]
-   [40 "XL"]
-   [10 "X"]
-   [9 "IX"]
-   [5 "V"]
-   [4 "IV"]
-   [1 "I"]])
+  (sorted-map-by >
+                 1000 "M"
+                 900 "CM"
+                 500 "D"
+                 400 "CD"
+                 100 "C"
+                 90 "XC"
+                 50 "L"
+                 40 "XL"
+                 10 "X"
+                 9 "IX"
+                 5 "V"
+                 4 "IV"
+                 1 "I"))
 
 (defn- amount->numerals [[output total] [amount numeral]]
   (let [times (quot total amount)]
