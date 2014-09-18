43e013347d975a8b7c9593544ab3ae49a52a67ba
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-09-17 22:43:02 -0500

    initial answer to binary in Ruby

---
+++ ruby/binary/binary.rb
@@ -0,0 +1,19 @@
+class Binary
+  def initialize(number)
+    @number = number
+  end
+
+  def to_decimal
+    @to_decimal ||= begin
+      return 0 if @number =~ /[^\d]/
+
+      sum = 0
+      @number
+        .reverse
+        .each_char.with_index do |digit, i|
+          sum += Integer(digit) * 2**i
+        end
+      sum
+    end
+  end
+end
