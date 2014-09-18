ffd32de43302759226ee1eb5263d27579a482240
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-09-17 22:59:06 -0500

    use a different strategy

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -7,13 +7,9 @@ class Binary
     @to_decimal ||= begin
       return 0 if @number =~ /[^01]/
 
-      sum = 0
-      @number
-        .reverse
-        .each_char.with_index do |digit, i|
-          sum += Integer(digit) * 2**i
-        end
-      sum
+      @number.chars.reduce(0) do |decimal, digit|
+        (decimal * 2) + Integer(digit)
+      end
     end
   end
 end
