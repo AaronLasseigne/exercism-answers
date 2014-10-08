57dbd05257f792e123033c3ca478f40355e4e8a8
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-10-07 19:46:08 -0500

    initial answer to crypto in ruby

---
+++ ruby/crypto-square/crypto.rb
@@ -0,0 +1,31 @@
+class Crypto
+  def initialize(message)
+    @message = message
+  end
+
+  def normalize_plaintext
+    @normalize_plaintext ||= @message.downcase.gsub(/[^0-9a-z]/, '')
+  end
+
+  def size
+    @size ||= Math.sqrt(normalize_plaintext.size).ceil
+  end
+
+  def plaintext_segments
+    @plaintext_segments ||= groups_of(normalize_plaintext, size)
+  end
+
+  def ciphertext
+    @ciphertext ||= size.times.with_object('') do |i, text|
+      text << plaintext_segments.map { |segment| segment[i] }.join
+    end
+  end
+
+  def normalize_ciphertext
+    @normalize_ciphertext ||= groups_of(ciphertext, 5).join(' ')
+  end
+
+  private def groups_of(text, count)
+    text.chars.each_slice(count).map(&:join)
+  end
+end
