0b88527c94ddcdfcfd97ca02285a6ff30d01fc97
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2025-08-09 15:19:22 -0500

    add rust difference_of_squares

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn square_of_sum(n: u32) -> u32 {
+    (1..=n).into_iter().sum::<u32>().pow(2)
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    (1..=n).into_iter().map(|num| num.pow(2)).sum()
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}
