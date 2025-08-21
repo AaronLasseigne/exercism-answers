8bff53f2713a6099d4f8738c3db08a9542d05b83
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2025-08-21 15:17:17 -0500

    update rust space-age

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -11,60 +11,28 @@ impl From<u64> for Duration {
 
 pub trait Planet {
     const SECONDS_PER_EARTH_YEAR: f64 = 31_557_600.0;
-
-    fn seconds_per_year() -> f64;
+    const PERIOD: f64;
 
     fn years_during(duration: &Duration) -> f64 {
-        duration.seconds as f64 / Self::seconds_per_year()
+        duration.seconds as f64 / (Self::SECONDS_PER_EARTH_YEAR * Self::PERIOD)
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! planet {
+    ( $name:ident, $period:literal ) => {
+        pub struct $name;
 
-impl Planet for Mercury {
-    fn seconds_per_year() -> f64 {
-        Self::SECONDS_PER_EARTH_YEAR * 0.2408467
+        impl Planet for $name {
+            const PERIOD: f64 = $period;
+        }
     }
 }
-impl Planet for Venus {
-    fn seconds_per_year() -> f64 {
-        Self::SECONDS_PER_EARTH_YEAR * 0.61519726
-    }
-}
-impl Planet for Earth {
-    fn seconds_per_year() -> f64 {
-        Self::SECONDS_PER_EARTH_YEAR
-    }
-}
-impl Planet for Mars {
-    fn seconds_per_year() -> f64 {
-        Self::SECONDS_PER_EARTH_YEAR * 1.8808158
-    }
-}
-impl Planet for Jupiter {
-    fn seconds_per_year() -> f64 {
-        Self::SECONDS_PER_EARTH_YEAR * 11.862615
-    }
-}
-impl Planet for Saturn {
-    fn seconds_per_year() -> f64 {
-        Self::SECONDS_PER_EARTH_YEAR * 29.447498
-    }
-}
-impl Planet for Uranus {
-    fn seconds_per_year() -> f64 {
-        Self::SECONDS_PER_EARTH_YEAR * 84.016846
-    }
-}
-impl Planet for Neptune {
-    fn seconds_per_year() -> f64 {
-        Self::SECONDS_PER_EARTH_YEAR * 164.79132
-    }
-}
+
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1.0);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
